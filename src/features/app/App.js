914a2fb913b137d314b77d860481486a4637c76c
Ariel Huang <ariel92911@gmail.com>
2022-09-17 16:29:51 +0800

    add CustomInputNumber component

--- src/features/app/App.js
+++ src/features/app/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
 import "./App.scss";
+import CustomInputNumber from "../customInputNumber/CustomInputNumber";
 
 export default function App() {
-  const [name, setName] = useState("React");
-
-  return <h1> Hello, {name} !</h1>;
+  return (
+    <CustomInputNumber
+      min={0}
+      max={20}
+      step={1}
+      name={"customNum"}
+      value={1}
+      disabled={false}
+    />
+  );
 }
---
+++ src/features/customInputNumber/CustomInputNumber.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import "./CustomInputNumber.scss";
+
+export default function CustomInputNumber(props) {
+  const { min, max, step, name, value, disabled } = props;
+  const [defaultValue, setDefaultValue] = useState(value);
+
+  const handleChangeNumber = (event) => {
+    setDefaultValue(event.target.value);
+  };
+
+  const handleBlurNumber = (event) => {
+    const num = event.target.value;
+
+    if (num > max || num < min || !num) {
+      alert(`請輸入 ${min} ~ ${max} 之間的數字`);
+      setDefaultValue(value);
+    }
+  };
+
+  const handleEMouseDown = (operate) => {
+    if (disabled) return;
+    setDefaultValue((defaultValue) => {
+      let result = Number(defaultValue) + operate;
+      if (result > max) return max;
+      if (result < min) return min;
+      return result;
+    });
+
+    let time = new Date();
+    setInterval(() => {
+      let nowTime = new Date();
+      if (nowTime.getTime() - time.getTime() > 500) {
+        setDefaultValue((defaultValue) => {
+          let result = Number(defaultValue) + operate;
+          if (result > max) return max;
+          if (result < min) return min;
+          return result;
+        });
+      }
+    }, 100);
+  };
+
+  const handleMouseUp = () => {
+    if (disabled) return;
+    for (let i = 1; i < 99999; i++) {
+      window.clearInterval(i);
+    }
+  };
+
+  return (
+    <div className="counter">
+      <button
+        className={`square btn ${defaultValue <= min ? "disabled" : ""}`}
+        onMouseDown={() => {
+          handleEMouseDown(-1 * step);
+        }}
+        onMouseUp={() => handleMouseUp()}
+      >
+        -
+      </button>
+
+      <input
+        type="number"
+        className={`square num ${disabled && "disabled"}`}
+        min={min}
+        max={max}
+        step={step}
+        name={name}
+        value={defaultValue}
+        disabled={disabled}
+        onChange={handleChangeNumber}
+        onBlur={handleBlurNumber}
+      />
+
+      <button
+        className={`square btn ${defaultValue >= max ? "disabled" : ""}`}
+        onMouseDown={() => {
+          handleEMouseDown(step);
+        }}
+        onMouseUp={() => handleMouseUp()}
+      >
+        +
+      </button>
+    </div>
+  );
+}
